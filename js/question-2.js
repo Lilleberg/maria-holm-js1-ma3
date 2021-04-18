d83bd5b3a84b7098b607307a75edf1ac8ea553a4
Maria Lilleberg Holm <maria.lilleberg@gmail.com>
2021-04-18 21:05:31 +0200

    doneeeeee srsly
    Api not working, need a key. I can no longer see what I am doing so i give up. i === 8 gave correct results, so i give up.

--- js/question-2.js
+++ js/question-2.js
@@ -1,10 +1,11 @@
-const url = "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating";
+const game_url = "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating";
+const proxy = "https://noroffcors.herokuapp.com/";
+const url = proxy + game_url;
 
 const gameContainer = document.querySelector(".gameContainer");
 
 async function games() {
   try {
-
     gameContainer.innerHTML = "";
 
     const response = await fetch(url);
@@ -12,12 +13,13 @@ async function games() {
     const data = results.results;
 
     for (let i = 0; i < data.length; i++) {
-
       const game = data[i];
 
       if (i === 8) break;
 
-      console.log(`Name: ${game.name} | Rating: ${game.rating}, nr. of tags: ${game.tags.length}`);
+      console.log(`${game.name}
+          Rating: ${game.rating}
+          Nr. of tags: ${game.tags.length}`);
 
       gameContainer.innerHTML +=
         `<div class="game">
