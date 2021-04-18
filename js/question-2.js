b6cd7e271b6deb88dbf12f8227b483de266c08c7
Maria Lilleberg Holm <maria.lilleberg@gmail.com>
2021-04-18 21:22:33 +0200

    Mjaumjau ferdig

--- js/question-2.js
+++ js/question-2.js
@@ -1,6 +1,4 @@
-const game_url = "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating";
-const proxy = "https://noroffcors.herokuapp.com/";
-const url = proxy + game_url;
+const url = "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating&key=4b791d2274ab41618b37d219015f3eee";
 
 const gameContainer = document.querySelector(".gameContainer");
 
@@ -13,13 +11,13 @@ async function games() {
     const data = results.results;
 
     for (let i = 0; i < data.length; i++) {
-      const game = data[i];
-
       if (i === 8) break;
 
-      console.log(`${game.name}
+      const game = data[i];
+
+      /*console.log(`${i} ${game.name}
           Rating: ${game.rating}
-          Nr. of tags: ${game.tags.length}`);
+          Nr. of tags: ${game.tags.length}`);*/
 
       gameContainer.innerHTML +=
         `<div class="game">
